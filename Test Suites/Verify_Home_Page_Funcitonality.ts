1a161316f9b12a7620080a3c080547349791b3b7
Het Truong <truongvanhetln@gmail.com>
2024-05-19 18:48:52 +0700

    update

--- Test Suites/Verify_Home_Page_Funcitonality.ts
+++ Test Suites/Verify_Home_Page_Funcitonality.ts
@@ -4,7 +4,7 @@
    <name>Verify_Home_Page_Funcitonality</name>
    <tag></tag>
    <delayBetweenInstances>0</delayBetweenInstances>
-   <executionMode>SEQUENTIAL</executionMode>
+   <executionMode>PARALLEL</executionMode>
    <maxConcurrentInstances>8</maxConcurrentInstances>
    <testSuiteRunConfigurations>
       <TestSuiteRunConfiguration>
