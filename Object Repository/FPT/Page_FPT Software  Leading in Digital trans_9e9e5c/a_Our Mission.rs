42fddaa2c3956feada4506ee36f18967262d6f32
Het Truong <truongvanhetln@gmail.com>
2024-05-20 23:04:34 +0700

    go

---
+++ Object Repository/FPT/Page_FPT Software  Leading in Digital trans_9e9e5c/a_Our Mission.rs
@@ -0,0 +1,138 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Our Mission</name>
+   <tag></tag>
+   <elementGuidId>eaa9dff9-84c0-4202-a028-d7d447ac6b60</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='our-missions']/div/div/div/div/div[4]/a</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div:nth-of-type(4) > a.fpt-cta-btn</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=link[name=&quot;Our Mission&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>8f4385cc-95d7-465f-9a69-203036b925de</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/about-us</value>
+      <webElementGuid>07034e40-3382-4ea7-b268-e25a382a18b4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>fpt-cta-btn</value>
+      <webElementGuid>09b8962f-efd9-4ead-b792-28cc1b4d0f7c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Our Mission</value>
+      <webElementGuid>b7bc85c2-2a73-4857-9e25-ddae33e01215</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;our-missions&quot;)/div[@class=&quot;component-content&quot;]/div[@class=&quot;component fsoft-promo our-mission&quot;]/div[@class=&quot;component-content&quot;]/div[@class=&quot;container-full-grid&quot;]/div[4]/a[@class=&quot;fpt-cta-btn&quot;]</value>
+      <webElementGuid>94bd9a27-fe09-4453-9cd9-ae8d5e003d9a</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='our-missions']/div/div/div/div/div[4]/a</value>
+      <webElementGuid>fc63373b-ec75-4cf7-9b7d-4cd48113423b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Our Mission')]</value>
+      <webElementGuid>2c733005-f75f-49f0-8b09-278ef54ad491</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Speed, Scale, Global Presence, Quality and Compliance.'])[1]/following::a[1]</value>
+      <webElementGuid>d20a1ef9-a04a-424a-a471-e4e341420627</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Our services'])[1]/preceding::a[1]</value>
+      <webElementGuid>f4adc0a5-fae7-4435-be94-906436e22b21</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Accompany clients through every step of the digital journey.'])[1]/preceding::a[1]</value>
+      <webElementGuid>9ba456b5-08db-47aa-aac6-887d7d619cdf</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Our Mission']/parent::*</value>
+      <webElementGuid>1ecb2c9e-6eb4-4626-9e2e-5686a4af83dd</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>(//a[contains(@href, '/about-us')])[14]</value>
+      <webElementGuid>2a3e9925-f623-479e-9bdb-04807f56413b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[4]/a</value>
+      <webElementGuid>af4422c8-9176-477a-8572-6cfa16e1cad8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '/about-us' and (text() = 'Our Mission' or . = 'Our Mission')]</value>
+      <webElementGuid>4eabcc41-fb9a-4935-b8c0-8b0c184cde25</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/FPT/Page_FPT Software  Leading in Digital trans_9e9e5c/div_WE ARE                              a c_6fe479.rs
@@ -0,0 +1,180 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_WE ARE                              a c_6fe479</name>
+   <tag></tag>
+   <elementGuidId>60cf53ba-a2d0-44ef-b377-2cd79880e423</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='our-missions']/div/div/div</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.component.fsoft-promo.our-mission > div.component-content</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>#our-missions div >> internal:has-text=&quot;WE ARE a comprehensive IT consulting &amp; service provider We are committed to deli&quot;i >> nth=2</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>82d15303-9f10-4154-b9bd-0b65328edbf2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>component-content</value>
+      <webElementGuid>12d65543-7bf1-4f4e-8389-91e3b7f334b8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+
+   WE ARE
+   
+      
+         
+            
+a 
+comprehensive
+
+IT consulting &amp;
+service provider
+         
+      
+      
+         
+      
+   
+   
+      
+         We are committed to delivering world-class technology solutions for complex business 
+ opportunities and challenges, leveraging from our five most compelling strengths of
+Speed, Scale, Global Presence, Quality and Compliance.
+      
+      
+         
+      
+   
+    Our Mission
+   
+        </value>
+      <webElementGuid>e60c26ef-3d74-4df7-b941-fef1ebc7e45c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;our-missions&quot;)/div[@class=&quot;component-content&quot;]/div[@class=&quot;component fsoft-promo our-mission&quot;]/div[@class=&quot;component-content&quot;]</value>
+      <webElementGuid>24ddd803-7c94-4bef-8407-2b97d9d0bc66</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='our-missions']/div/div/div</value>
+      <webElementGuid>f26ed42d-3e92-4170-a2bc-f3eacc986f0f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='FPT Software Listed in Gartner® Report For Public Cloud Services'])[4]/following::div[5]</value>
+      <webElementGuid>6bd9a7e4-988a-40f4-8158-23a48d6c2427</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//main/div/div[2]/div/div/div</value>
+      <webElementGuid>ccf3909c-8a23-4240-ae5c-052c35907ce9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+
+   WE ARE
+   
+      
+         
+            
+a 
+comprehensive
+
+IT consulting &amp;
+service provider
+         
+      
+      
+         
+      
+   
+   
+      
+         We are committed to delivering world-class technology solutions for complex business 
+ opportunities and challenges, leveraging from our five most compelling strengths of
+Speed, Scale, Global Presence, Quality and Compliance.
+      
+      
+         
+      
+   
+    Our Mission
+   
+        ' or . = '
+
+   WE ARE
+   
+      
+         
+            
+a 
+comprehensive
+
+IT consulting &amp;
+service provider
+         
+      
+      
+         
+      
+   
+   
+      
+         We are committed to delivering world-class technology solutions for complex business 
+ opportunities and challenges, leveraging from our five most compelling strengths of
+Speed, Scale, Global Presence, Quality and Compliance.
+      
+      
+         
+      
+   
+    Our Mission
+   
+        ')]</value>
+      <webElementGuid>d65f7d50-86f7-47b2-b916-f6b32cebf370</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
