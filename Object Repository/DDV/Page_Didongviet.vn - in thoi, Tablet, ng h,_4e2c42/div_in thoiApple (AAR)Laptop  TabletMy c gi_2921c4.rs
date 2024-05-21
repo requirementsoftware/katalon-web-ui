e39485a571a16c6e935a3d75ba5760f5f609018e
Het Truong <truongvanhetln@gmail.com>
2024-05-21 14:43:45 +0700

    go now

---
+++ Object Repository/DDV/Page_Didongviet.vn - in thoi, Tablet, ng h,_4e2c42/div_in thoiApple (AAR)Laptop  TabletMy c gi_2921c4.rs
@@ -0,0 +1,114 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_in thoiApple (AAR)Laptop  TabletMy c gi_2921c4</name>
+   <tag></tag>
+   <elementGuidId>10d3ab77-57fd-4ab1-b0ac-ec779d3afbfb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='__next']/main/div[2]/main/div/div/div/div/div</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:text=&quot;Điện thoạiApple (AAR)Laptop / TabletMáy cũ giá rẻPhụ kiệnSức khoẻ, Làm đẹpThiết &quot;i</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>3d2bd85d-2434-45d1-a648-d5511a457915</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>dropend relative mt-2 flex w-full flex-col justify-between rounded-lg bg-white p-1 group-hover:text-ddv</value>
+      <webElementGuid>b8b6dfb6-f5d8-49a3-ae34-7b2de36a80e9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Điện thoạiApple (AAR)Laptop / TabletMáy cũ giá rẻPhụ kiệnSức khoẻ, Làm đẹpThiết bị đeo tayThiết bị âm thanhThu cũ đổi mớiĐặt vé máy bayKhuyến mãiCông nghệ 24HTuyển dụng</value>
+      <webElementGuid>99328e34-d54c-472b-9f13-1353b2d68fd1</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;__next&quot;)/main[@class=&quot;__className_8248c3&quot;]/div[@class=&quot;relative bg-bgddv antialiased&quot;]/main[@class=&quot;flex w-full flex-col items-center justify-start py-1&quot;]/div[@class=&quot;container w-full&quot;]/div[@class=&quot;container z-0 antialiased&quot;]/div[@class=&quot;group relative mb-2 min-h-[410px] antialiased  max-md:min-h-[327px] max-md:overflow-hidden max-md:px-2 md:grid md:grid-cols-12 md:gap-2&quot;]/div[@class=&quot;col-span-2 max-md:hidden&quot;]/div[@class=&quot;dropend relative mt-2 flex w-full flex-col justify-between rounded-lg bg-white p-1 group-hover:text-ddv&quot;]</value>
+      <webElementGuid>279acf6a-a822-4d94-914d-ade9bb0bb379</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='__next']/main/div[2]/main/div/div/div/div/div</value>
+      <webElementGuid>f8b81e85-3272-476e-8bd0-bbaa9110ef7e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Đăng ký'])[1]/following::div[12]</value>
+      <webElementGuid>ebccb4ef-205d-4301-aefb-741821cc3d2b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Đăng nhập'])[1]/following::div[13]</value>
+      <webElementGuid>d62a9f0a-47b1-4ce3-acce-bbec2a7ba1c9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='DEAL MÁY CŨ'])[1]/preceding::div[264]</value>
+      <webElementGuid>5fc63dca-2cf9-4a0c-b7f0-19d12d6f9829</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='ĐIỆN THOẠI | PHỤ KIỆN GIÁ RẺ'])[1]/preceding::div[265]</value>
+      <webElementGuid>5cb55eb6-f014-458d-a79a-d3fb4f439fa4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/main/div/div/div/div/div</value>
+      <webElementGuid>390ea6b1-8bb4-4142-a9e8-0ebd6e38b6a8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Điện thoạiApple (AAR)Laptop / TabletMáy cũ giá rẻPhụ kiệnSức khoẻ, Làm đẹpThiết bị đeo tayThiết bị âm thanhThu cũ đổi mớiĐặt vé máy bayKhuyến mãiCông nghệ 24HTuyển dụng' or . = 'Điện thoạiApple (AAR)Laptop / TabletMáy cũ giá rẻPhụ kiệnSức khoẻ, Làm đẹpThiết bị đeo tayThiết bị âm thanhThu cũ đổi mớiĐặt vé máy bayKhuyến mãiCông nghệ 24HTuyển dụng')]</value>
+      <webElementGuid>713f049e-ce63-476d-b3e8-cae6aa553634</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
