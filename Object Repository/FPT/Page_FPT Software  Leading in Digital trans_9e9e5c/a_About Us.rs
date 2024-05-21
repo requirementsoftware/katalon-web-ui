391797f8d95b36277c43a7fb6915091ba51ebb91
Het Truong <truongvanhetln@gmail.com>
2024-05-21 08:57:32 +0700

    go

--- Object Repository/FPT/Page_FPT Software  Leading in Digital trans_9e9e5c/a_About Us.rs
+++ Object Repository/FPT/Page_FPT Software  Leading in Digital trans_9e9e5c/a_About Us.rs
@@ -7,18 +7,18 @@
    <selectorCollection>
       <entry>
          <key>XPATH</key>
-         <value>//div[@id='footer']/div/div/div/div/div/div/div/div/nav/ul/li/a</value>
+         <value>//div[@id='header']/div/div/div[2]/div/div[3]/div/nav/ul/li[4]/a</value>
       </entry>
       <entry>
          <key>CSS</key>
-         <value>ul.clearfix > li.level1.item0.odd.first.rel-level1 > a</value>
+         <value>li.rel-level1.level1.item3.even.last.nav-main__first--li.has-sub > a.active</value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
    <smartLocatorCollection>
       <entry>
          <key>SMART_LOCATOR</key>
-         <value>#footer >> internal:role=link[name=&quot;About Us&quot;i]</value>
+         <value>#header >> internal:role=navigation >> internal:role=link[name=&quot;About Us&quot;i]</value>
       </entry>
    </smartLocatorCollection>
    <smartLocatorEnabled>false</smartLocatorEnabled>
@@ -29,7 +29,7 @@
       <name>tag</name>
       <type>Main</type>
       <value>a</value>
-      <webElementGuid>78fcba16-af1a-43bd-9a32-7dbb04efae5f</webElementGuid>
+      <webElementGuid>58b10b37-d635-46c2-b95d-6dbbf2f36a49</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
@@ -37,26 +37,128 @@
       <name>href</name>
       <type>Main</type>
       <value>/about-us</value>
-      <webElementGuid>de07b0fd-1ba2-41d9-9fcf-a743a0acc7cd</webElementGuid>
+      <webElementGuid>6bc80438-39c2-4a67-b81f-b8fc54b2ae15</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>text</name>
       <type>Main</type>
-      <value>About Us</value>
-      <webElementGuid>3c7b4381-d98e-4c25-95ad-fd4304f09ea1</webElementGuid>
+      <value> 
+    About Us
+</value>
+      <webElementGuid>10ef05bb-d7ba-4741-9fe0-505e25fdcbed</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>id(&quot;footer&quot;)/div[@class=&quot;component fsoft-container bgr-grey-shade-color p-t-40-mb p-t-56&quot;]/div[@class=&quot;component-content&quot;]/div[@class=&quot;component fsoft-container container-full-grid&quot;]/div[@class=&quot;component-content&quot;]/div[@class=&quot;row component fsoft-column-splitter&quot;]/div[@class=&quot;col-sm-12 col-lg-8 order-2-mb&quot;]/div[@class=&quot;component navigation navigation-title footer-navigation initialized&quot;]/div[@class=&quot;component-content&quot;]/nav[1]/ul[@class=&quot;clearfix&quot;]/li[@class=&quot;level1 item0 odd first rel-level1&quot;]/a[1]</value>
-      <webElementGuid>48d7a07c-f5ed-4503-8d3e-db0134040746</webElementGuid>
+      <value>id(&quot;header&quot;)/div[@class=&quot;component fsoft-container&quot;]/div[@class=&quot;component-content&quot;]/div[@class=&quot;component fsoft-container bgr-white header-container-second&quot;]/div[@class=&quot;component-content&quot;]/div[@class=&quot;component main-navigation&quot;]/div[@class=&quot;component-content&quot;]/nav[@class=&quot;nav-main&quot;]/ul[@class=&quot;nav-main__menu d-flex align-items-center&quot;]/li[@class=&quot;rel-level1  level1 item3 even last nav-main__first--li has-sub&quot;]/a[@class=&quot;active&quot;]</value>
+      <webElementGuid>032ae122-e672-4d51-ad27-7a85f3527fe3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-hover-target</name>
+      <type>Main</type>
+      <value>{5B690C28-2812-44C7-A873-9FDE5262F591}</value>
+      <webElementGuid>cbeb4a00-6332-4bbd-8ec7-550b66da0dae</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-page-active</name>
+      <type>Main</type>
+      <value>false</value>
+      <webElementGuid>5c32604f-1bf8-4162-936f-82999dc7172c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>active</value>
+      <webElementGuid>c2a9ac14-d901-4bb3-84b6-41852eb07a49</webElementGuid>
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='header']/div/div/div[2]/div/div[3]/div/nav/ul/li[4]/a</value>
+      <webElementGuid>6f93e962-0099-4bd3-b013-5c7bd304081f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'About Us')]</value>
+      <webElementGuid>c252b338-8c27-4059-89c4-93029d329099</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Let us accompany you'])[1]/following::a[1]</value>
+      <webElementGuid>2031ca2a-ba6c-4b1a-8a27-c9715b922af1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='MISSION TO'])[1]/preceding::a[2]</value>
+      <webElementGuid>383f699f-111e-40e7-8e6b-1b82958a6cd1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Accompany'])[1]/preceding::a[2]</value>
+      <webElementGuid>1282798e-442c-446a-bd3d-0d15e6b7ef88</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='About Us']/parent::*</value>
+      <webElementGuid>b3126269-7d2d-411e-a303-2a0f713ffb5e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, '/about-us')]</value>
+      <webElementGuid>02ca01d4-3aac-472a-9024-53c268895da2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//nav/ul/li[4]/a</value>
+      <webElementGuid>cb89e4b9-394c-46ef-be4f-6f4cedbf5c09</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '/about-us' and (text() = ' 
+    About Us
+' or . = ' 
+    About Us
+')]</value>
+      <webElementGuid>28b6840e-18ff-42f2-9711-403112ddad5f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:idRelative</name>
       <type>Main</type>
